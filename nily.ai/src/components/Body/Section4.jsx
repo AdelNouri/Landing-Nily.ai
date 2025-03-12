6ec290fb2bc6192473f3428642b39a61150ff42f
Adel Nouri <adelnouri0231@gmail.com>
2025-03-13 03:14:15 +0330

    Responsiveness section 4 is complete.

--- nily.ai/src/components/Body/Section4.jsx
+++ nily.ai/src/components/Body/Section4.jsx
@@ -12,7 +12,7 @@ const Section4 = () => {
           className=""
         >
           <p className="font-semibold text-[#FC6423]">OUR FEATURES</p>
-          <div className="font-bold text-3xl md:text-4xl lg:text-5xl text-slate-900 mt-4">
+          <div className="font-bold text-4xl lg:text-5xl text-slate-900 mt-4">
             <p>Boost your productivity with</p>
             <p> AI-powered assistance</p>
           </div>
@@ -24,7 +24,7 @@ const Section4 = () => {
           viewport={{ once: true, amount: 0.2 }}
           className=""
         >
-          <div className="grid grid-cols-4 gap-6 my-20 w-320">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-20 w-full px-3 sm:w-[608px] md:w-[736px] lg:w-[992px] xl:w-320">
             <div className="bg-white px-8 py-8 rounded-2xl cursor-pointer hover:[box-shadow:0px_10px_12px_rgba(0,0,0,0.1)] transition duration-300">
               <div className="flex justify-start items-center">
                 <div className="bg-[#F4F1ED] rounded-xl p-1.5 text-[#FC6423]">
